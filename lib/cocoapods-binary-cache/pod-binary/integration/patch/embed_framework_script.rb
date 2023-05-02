aa4c4c97cc0735a8be21f8044d440071c38d3d04
Scott Southerland <scottasoutherland@thumbtack.com>
2023-05-01 21:07:47 -0700

    Fix issue with XC 14.3

--- lib/cocoapods-binary-cache/pod-binary/integration/patch/embed_framework_script.rb
+++ lib/cocoapods-binary-cache/pod-binary/integration/patch/embed_framework_script.rb
@@ -10,26 +10,8 @@ module Pod
       old_method = instance_method(:script)
       define_method(:script) do
         script = old_method.bind(self).call
-        patch = <<-SH.strip_heredoc
-          #!/bin/sh
-          # ---- this is added by cocoapods-binary ---
-          # Readlink cannot handle relative symlink well, so we override it to a new one
-          # If the path isn't an absolute path, we add a realtive prefix.
-          old_read_link=`which readlink`
-          readlink () {
-            path=`$old_read_link "$1"`;
-            if [ $(echo "$path" | cut -c 1-1) = '/' ]; then
-              echo $path;
-            else
-              echo "`dirname $1`/$path";
-            fi
-          }
-          # ---
-        SH
-
         # patch the rsync for copy dSYM symlink
         script = script.gsub "rsync --delete", "rsync --copy-links --delete"
-        patch + script
       end
     end
   end
